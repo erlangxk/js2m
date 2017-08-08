ecc1ada507c94c4d5767b76373eae119845cede9
erlangxk <simon.xikun@gmail.com>
2017-08-08 11:19:35 +0800

    add todo, count active todo

--- react/todolist/src/App.tsx
+++ react/todolist/src/App.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-
 import { TodoListU } from './components/TodoList';
-
 import { item, TodoItem } from './components/TodoItem';
+
+const uuidv1 = require('uuid/v1');
 
 class App extends React.Component<{}, { items: TodoItem[] }> {
   constructor() {
@@ -11,6 +11,17 @@ class App extends React.Component<{}, { items: TodoItem[] }> {
       items: [item('idsfsfsfsf', 'text1', true), item('id2', 'textssssssssssssss', false)]
     };
     this.handleClick = this.handleClick.bind(this);
+  }
+
+  numOfActiveItems = () => {
+    function cb(accu: number, cv: TodoItem) {
+      if (cv.completed) {
+        return accu;
+      } else {
+        return accu + 1;
+      }
+    }
+    return this.state.items.reduce(cb, 0);
   }
 
   handleClick(todoItem: TodoItem) {
@@ -26,10 +37,36 @@ class App extends React.Component<{}, { items: TodoItem[] }> {
     this.setState({ items: newitems });
   }
 
+  handleAddTodo = (value: string) => {
+    let items = this.state.items.slice();
+    items.push(item(uuidv1(), value, false));
+    this.setState({ items: items });
+  }
+
+  handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.keyCode === 13) {
+      let input = document!.getElementById('newTodo') as HTMLInputElement;
+      let text = input.value;
+      if (text != null) {
+        this.handleAddTodo(text);
+        input.value = '';
+      }
+    }
+  }
+
   render() {
     return (
       <div >
+        <input
+          id="newTodo"
+          type="text"
+          name="newTodo"
+          placeholder="What needs to be done?"
+          onKeyDown={this.handleEnter}
+        />
         <TodoListU items={this.state.items} onClick={this.handleClick} />
+
+        <span>{this.numOfActiveItems()} left</span>
       </div >
     );
   }
