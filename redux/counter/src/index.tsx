4dcce26c5023148452ccf170274ab6f28ac46b45
erlangxk <simon.xikun@gmail.com>
2017-08-25 18:42:19 +0800

    with redux-observable

--- redux/counter/src/index.tsx
+++ redux/counter/src/index.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { reducer } from './reducers'
-import { Counter } from './components/Counter'
+import { createStore, applyMiddleware } from 'redux';
+import { reducer } from './reducers';
+import { Counter } from './components/Counter';
 
-import { connect, Provider } from 'react-redux'
+import { connect, Provider } from 'react-redux';
+import { createEpicMiddleware, combineEpics } from 'redux-observable';
+import 'rxjs';
 
-const store = createStore(reducer, 0);
+const asyncEpic = (action$: any) => action$.ofType('INCREMENT_ASYNC').delay(1000).mapTo({ type: 'INCREMENT' });
+const oddEpic = (action$: any, store: any) => action$.ofType('INCREMENT_IF_ODD').filter(() => store.getState() % 2 == 1).mapTo({ type: 'INCREMENT' });
+const rootEpic = combineEpics(asyncEpic, oddEpic);
+
+const middleweare = createEpicMiddleware(rootEpic);
+
+const store = createStore(reducer, 0, applyMiddleware(middleweare));
 function mapStateToProps(state: number) {
     return { value: state };
 }
@@ -15,16 +23,13 @@ function mapDispatchToProps(dispatch: any) {
     return {
         onIncrement: () => dispatch({ type: 'INCREMENT' }),
         onDecrement: () => dispatch({ type: 'DECREMENT' }),
-        incrementAsync: function () {
-            setTimeout(() => dispatch({ type: 'INCREMENT' }), 3000);
-        },
-        incrementIfOdd: function (value: number) {
-            if (value % 2 != 0) dispatch({ type: 'INCREMENT' })
-        }
+        incrementAsync: () => dispatch({ type: 'INCREMENT_ASYNC' }),
+        incrementIfOdd: (value: number) => dispatch({ type: 'INCREMENT_IF_ODD' })
     }
 }
 
 const CounterX = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
 
 ReactDOM.render(
     <Provider store={store}>
